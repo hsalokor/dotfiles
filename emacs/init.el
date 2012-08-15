005e96cc371a41cf621aee50315698f16f54702a
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2012-08-15 18:17:48 +0300

    Switch to melpa repository, and add nrepl to packages

--- emacs/init.el
+++ emacs/init.el
@@ -3,7 +3,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+    '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
@@ -17,6 +17,7 @@
     rainbow-delimiters
     paredit
     clojure-mode
+    nrepl
     starter-kit
     starter-kit-lisp
     starter-kit-bindings))
