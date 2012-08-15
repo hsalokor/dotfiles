b1a5ec001982e4fdf55e7afe00670103a51ec62e
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2012-08-15 18:15:00 +0300

    Clean up mac-specific stuff rom hsalokor.el and really fix the clojure indent

--- emacs/hsalokor.el
+++ emacs/hsalokor.el
@@ -1,15 +1,6 @@
-(set-default-font "Menlo-15")
+;(set-default-font "Menlo-15")
 (load-theme 'deeper-blue)
 (tool-bar-mode -1)
-
-(add-to-list 'load-path "~/.cabal/share/ghc-mod-1.10.15/")
-(autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
-
-; Mac brace fix
-(setq default-input-method "MacOSX")
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'none)
 
 (require 'evil)
 (require 'sr-speedbar)
@@ -21,7 +12,8 @@
 (evil-mode 1)
 
 ; Fix lisp indent
-(setq lisp-indent-offset 2)
+(setq indent-tabs-mode nil)
+(setq tab-width 2)
 
 ; Add lein to path
 (setenv "PATH" (concat "/home/hsalokor/bin:" (getenv "PATH")))
