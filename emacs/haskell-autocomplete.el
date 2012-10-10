863f82e78d05c3d89406cdb1a4b4ef9c3bb01dd0
Harri Salokorpi <hsalokor@iki.fi>
2012-10-11 02:34:50 +0300

    Add initial version of ghc-mod and autocomplete integration. TODO: extract and submit upstream?

---
+++ emacs/haskell-autocomplete.el
@@ -0,0 +1,12 @@
+(require 'ghc-comp)
+
+(defun haskell-complete ()
+  (let* ((end (point))
+         (symbols (ghc-select-completion-symbol))
+         (beg (ghc-completion-start-point))
+         (pattern (buffer-substring-no-properties beg end)))
+    (list (sort (all-completions pattern symbols) 'string<))))
+
+(defvar ac-source-ghc-mod
+  '((init . ghc-init)
+    (candidates . (haskell-complete))))
