f81186249831755a8386b9d9ae41a8351b42c8ee
Harri Salokorpi <hsalokor@iki.fi>
2012-10-11 21:13:08 +0300

    Add clojure-autocomplete.el

---
+++ emacs/clojure-autocomplete.el
@@ -0,0 +1,10 @@
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
+(defun set-auto-complete-as-completion-at-point-function () (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(provide 'clojure-autocomplete)
