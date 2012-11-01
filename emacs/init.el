9d85b1cd7f5771a29d5872fba40c8196eed8f035
Harri Salokorpi <hsalokor@iki.fi>
2012-11-01 15:22:18 +0200

    Add marmalade to package repository list (geiser is not in melpa repo)

--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,9 @@
 
 (require 'package)
 (add-to-list 'package-archives
-    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
